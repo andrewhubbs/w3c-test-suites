0cbc232e6cf0bf64d01c478f8ad3ce632679ade6
Andrew Hubbs <pair+andrew@rally.org>
2012-06-30 15:12:57 -0700

    Added a user agents section. Updated some styling and added nab bar.

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,9 +2,15 @@
   app.View = {
     baseURL : "http://w3c-test.org/framework/api/",
     suites : null,
+    agents : null,
 
     init : function () {
       $.get(this.baseURL + "testsuites", this.handleTestSuitesResponse);
+      $.get(this.baseURL + "known-user-agents", this.handleUserAgentsResponse);
+      $(".navbar a").click(function (e) {
+        $($(this).attr("href")).removeClass("hide").siblings().addClass("hide");
+        e.preventDefault();
+      });
     },
 
     handleTestSuitesResponse : function (data) {
@@ -13,16 +19,46 @@
         $(".suites-list").append("<li><a href='" + app.View.baseURL + "result/" + suite.testsuite + "?scope=full'>" + suite.title + "</a></li>");
       });
 
-      $(".suites-list li a").on("click", function (e) {
+      $(".suites-list li a").click(function (e) {
         $(this).parent().addClass("active").siblings().removeClass("active");
         $.get($(this).attr("href"), app.View.handleTestCasesResponse);
         e.preventDefault();
       })
     },
 
-    handleTestCasesResponse : function (data) {
-      console.log(data);
+    handleUserAgentsResponse : function (data) {
+      app.View.agents = data;
+      _.each(app.View.agents, function (agent) {
+        $(".user-agents-list").append("<li><a href='#" + agent.engine + "'>" + agent.engine + "</a></li>");
 
+        var browserMarkup = "<ul>";
+        _.each(agent.browsers, function (browser) {
+          var uasMarkup = "<ul class='hide'>";
+          _.each(browser.platforms, function (platform) {
+            _.each(platform.uas, function (ua) {
+              uasMarkup += "<li>" + ua.useragent + "</li>";
+            });
+          });
+          uasMarkup += "</ul>";
+          browserMarkup += "<li><a href='#'>" + browser.browser + "</a>" + uasMarkup + "</li>";
+        });
+        browserMarkup += "</ul>";
+        $(".user-agents-details-list").append("<div id='" + agent.engine + "' class='hide'>" + browserMarkup + "</div>");
+      });
+
+      $(".user-agents-list li a").click(function (e) {
+        $(this).parent().addClass("active").siblings().removeClass("active");
+        $($(this).attr("href")).removeClass("hide").siblings().addClass("hide");
+        e.preventDefault();
+      });
+
+      $(".user-agents-details-list a").click(function (e) {
+        $(this).next("ul").toggleClass("hide");
+        e.preventDefault();
+      });
+    },
+
+    handleTestCasesResponse : function (data) {
       $(".results-list").html("");
 
       _.each(data, function (result) {
@@ -33,7 +69,7 @@
           _.each(_.keys(result.results_by_engine[engineKey]), function (versionKey) {
             var passResult = result.results_by_engine[engineKey][versionKey].result;
             var labelType = (passResult === "pass" ? "label-success" : (passResult === "fail" ? "label-important" : "label-warning"));
-            engineVersionsMarkup += "<li><span>" + versionKey + "</span> <span class='label " + labelType + "'>" + passResult + "</span></li>"
+            engineVersionsMarkup += "<li><span>" + result.results_by_engine[engineKey][versionKey].useragent_id + " (" + versionKey + ")</span> <span class='label " + labelType + "'>" + passResult + "</span></li>"
           });
           resultsMarkup += engineVersionsMarkup + "</ul></li>";
         });
@@ -41,7 +77,7 @@
 
         $(".results-list").append("<li>" +
           "<a href='#result_" + result.id + "' class='visibilty-toggler'>" + result.title + "</a>" +
-          "<a href='"+result.formatURIs.html+"' target='_blank' class='test-link'>View test</a>" +
+          "<a href='" + result.formatURIs.html + "' target='_blank' class='test-link'>View test</a>" +
           "<div id='result_" + result.id + "' class='hide'>" +
           resultsMarkup +
           "</div>" +
